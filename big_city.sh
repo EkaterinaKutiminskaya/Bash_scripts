1b3a7d997a40da7dcbb04b89d9f1d99037ffdca8
Ekaterina Kutiminskaya <katerinakyt@gmail.com>
2023-06-15 01:43:14 +0700

    новая структура, перевод в десятичное перед сравнением

--- big_city.sh
+++ big_city.sh
@@ -1,54 +1,48 @@
 #!/bin/bash
 
-echo "Привет!" 
-echo "Cколько знаешь городов России с населением более 1 млн. человек?"
-
 city=16
 all_city=1118
 
 function numberNotAdded () {
-					echo -e "\nТы не ввел число, пока!" 
-					exit
+	echo -e "\nТы не ввел число, пока!" 
+	exit
 }
 
+time=20
+
+echo "Привет!" 
+echo "Cколько знаешь городов России с населением более 1 млн. человек?"
 echo -n "У тебя есть немного времени на подумать. Напиши свой вариант: "
 
-if read -t 20 -n 4 guess; then
-	function checkNumber () {
-		while ! [[ "$guess" =~ ^[0-9]+$ ]]
-		do
-			if read -t 10 -n 4 -p " Нужно ввести целое число больше 0! " guess
-			then echo ""
-			else
-				numberNotAdded
-			fi
-		done
-	}
-	checkNumber
-	while [[ $guess -ne $city ]]
-	do
-		if [[ $guess -lt $city ]]; then 
-			if read -t 10 -n 4 -p " Городов больше, попробуй еще раз! " guess
-			then checkNumber
-			else
-				numberNotAdded
-			fi	
-		elif [[ $guess -gt $city && $guess -le $all_city ]]; then 
-			if read -t 10 -n 4 -p " Слишком много, попробуй еще раз :) " guess
-			then checkNumber
-			else
-				numberNotAdded
-			fi
-		else 
-			if read -t 10 -n 4 -p " Введи целое число до $all_city, столько городов есть в РФ " guess
-			then 
-				checkNumber
-			else
-				numberNotAdded
-			fi
+while true; do
+	if read -t $time -n 4 guess; then
+		time=10
+		if ! [[ $guess =~ ^[0-9]+$ ]]; then
+			echo "  Нужно ввести целое число больше 0! "
+			continue
 		fi
-	done
-	echo "Все верно, по данным Википедии, на 2023г городов 16. Пока!"
-else
-	numberNotAdded
-fi
+		
+		if [[ $[10#$guess] -eq $city ]]; then 
+			echo "  Все верно, по данным Википедии, на 2023г городов 16. Пока!"
+			exit
+		fi
+
+		if [[ $[10#$guess] -lt $city ]]; then 
+			echo "  Городов больше, попробуй еще раз!"
+			continue
+		fi
+
+		if [[ $[10#$guess] -gt $city && $[10#$guess] -le $all_city ]]; then 
+			echo "  Слишком много, попробуй еще раз :)"
+			continue
+		fi
+
+		if [[ $[10#$guess] -gt $all_city ]]; then 
+			echo "  Введи целое число до $all_city, столько городов есть в РФ "
+			continue
+		fi
+		
+	else
+		numberNotAdded
+	fi
+done
